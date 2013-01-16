5b63812ce9c598306b523adaf2d4260ca75ee0e7
straup <straup@gmail.com>
2013-01-15 22:59:50 -0500

    new front page

---
+++ javascript/loopr.js
@@ -0,0 +1,31 @@
+var loopr_url = 'http://info.aaronland.loopr.s3.amazonaws.com/info.aaronland.loopr.json';
+
+function loopr_init(){
+	loopr_load();
+}
+
+function loopr_load(){
+
+	var onsuccess = function(rsp){
+		loopr_show(rsp['loops']);
+	};
+
+	$.ajax({'url': loopr_url, 'success': onsuccess, 'dataType': 'json' });
+}
+
+function loopr_show(loops){
+
+	var img = loops.pop()
+	console.log(img);
+
+	$("#offstage").attr("src", img);
+
+	$("#offstage").load(function(){
+		$("#loopr").attr("src", img);
+		$("#offstage").attr("src", "");
+	});
+
+	setTimeout(function(){
+		(loops.length) ? loopr_show(loops) : loopr_load();
+	}, 10000);
+}
