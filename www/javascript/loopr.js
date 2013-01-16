272e90796d66c07f00607c2df4b30ddbe887632b
straup <straup@gmail.com>
2013-01-15 22:37:47 -0500

    css

--- www/javascript/loopr.js
+++ www/javascript/loopr.js
@@ -3,40 +3,27 @@ function loopr_init(){
 }
 
 function loopr_load(){
-	$.ajax({'url': loopr_url, 'success': loopr_show, 'dataType': 'json' });
-}
 
-function loopr_show(rsp){
-
-	var loops = rsp['loops']
-	var count = loops.length;
-
-	var i = 0;
-
-    /* BROKEN ... */
-
-	var load = function(){
-
-		var current = loops[i];
-
-		if (! current){
-			loopr_load();
-			return;
-		}
-
-		i++;
-		loopr_load(current);
-		setTimeout(load, 10000);
+	var onsuccess = function(rsp){
+		loopr_show(rsp['loops']);
 	};
 
-	load();
+	$.ajax({'url': loopr_url, 'success': onsuccess, 'dataType': 'json' });
 }
 
-function loopr_load(img){
+function loopr_show(loops){
+
+	var img = loops.pop()
+	console.log(img);
 
 	$("#offstage").attr("src", img);
 
 	$("#offstage").load(function(){
 		$("#loopr").attr("src", img);
+		$("#offstage").attr("src", "");
 	});
+
+	setTimeout(function(){
+		(loops.length) ? loopr_show(loops) : loopr_load();
+	}, 10000);
 }
