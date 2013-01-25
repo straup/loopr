2e0d58dbbe719d8751edb682a3adb3d5986e1219
straup <straup@gmail.com>
2013-01-25 16:04:37 -0500

    do not try to import fseventsobserver if this not osx

--- bin/webcam-fswebcam.sh
+++ bin/webcam-fswebcam.sh
@@ -2,13 +2,13 @@
 
 OUT=$1
 
-DEVICE=/dev/video1
+DEVICE=/dev/video0
 RESOLUTION=1280x720
 FSWEBCAM=`which fswebcam`
 
 while [ 1 ]
 do
     NOW=`date +%s`
-    ${FSWEBCAM} --quiet --no-banner -d ${DEVICE} -r ${RESOLUTION} ${OUT}/webcam-${NOW}.jpg
+    ${FSWEBCAM} --no-banner -d ${DEVICE} -r ${RESOLUTION} ${OUT}/webcam-${NOW}.jpg
     sleep 5
 done
