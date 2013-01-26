40ddb97ad49ff7a438d2c76da2bba5068cf58fe5
straup <straup@gmail.com>
2013-01-26 13:58:10 -0500

    make webcam-opencv work with a config file

--- bin/webcam-opencv.py
+++ bin/webcam-opencv.py
@@ -8,21 +8,23 @@ import time
 import logging
 import os.path
 import logging
+import ConfigParser
 
 import cv2
 
-def capture(opts):
+def capture(camera, outdir):
 
     vidcap = cv2.VideoCapture()
-    vidcap.open(opts.camera)
+    vidcap.open(camera)
 
     retval, image = vidcap.retrieve()
     vidcap.release()
     
     now = int(time.time())
-    fname = "%s-camera-%s.jpg" % (now, opts.camera)
+    fname = "%s-camera-%s.jpg" % (now, camera)
 
-    path = os.path.join(opts.out, fname)
+    path = os.path.join(outdir, fname)
+    print path
 
     cv2.imwrite(path, image)
     return path
@@ -32,10 +34,13 @@ if __name__ == '__main__':
     import optparse
 
     parser = optparse.OptionParser()
-    parser.add_option("-T", "--timer", dest="timer", help="...", default=0, type='int')
-    parser.add_option("-o", "--out", dest="out", help="", default=None)
-    parser.add_option("-c", "--camera", dest="camera", help="", default=0, type='int')
+    parser.add_option('-c', '--config', dest='config', action='store', help='path to a loopr config file - see source code for a sample config')
     parser.add_option("-v", "--verbose", dest="verbose", action="store_true", help="enable chatty logging; default is false", default=False)
+
+    # MAYBE: allow CLI flags to override the config file?
+    # parser.add_option("-T", "--timer", dest="timer", help="...", default=0, type='int')
+    # parser.add_option("-o", "--out", dest="out", help="", default=None)
+    # parser.add_option("-c", "--camera", dest="camera", help="", default=0, type='int')
 
     (opts, args) = parser.parse_args()
 
@@ -44,11 +49,18 @@ if __name__ == '__main__':
     else:
         logging.basicConfig(level=logging.INFO)
 
+    cfg = ConfigParser.ConfigParser()
+    cfg.read(opts.config)
+
+    camera = int(cfg.get('webcam-opencv', 'camera'))
+    outdir = cfg.get('webcam-opencv', 'out')
+    timeout = cfg.get('webcam-opencv', 'timeout')
+
     while True:
 
         try:
-            logging.debug("taking a picture with camera %s" % opts.camera)
-            path = capture(opts)
+            logging.debug("taking a picture with camera %s" % camera)
+            path = capture(camera, outdir)
         except Exception, e:
             logging.error("unable to time a picture, because: %s" % e)
             time.sleep(0.5)
@@ -56,11 +68,11 @@ if __name__ == '__main__':
 
         # cat *.jpg | ffmpeg -f image2pipe -r 1/.5 -vcodec mjpeg -i - -vcodec libx264 out.mp4
 
-        if not opts.timer:
+        if not timeout:
             break
 
-        logging.debug("sleep for %s seconds" % opts.timer)
-        time.sleep(float(opts.timer))
+        logging.debug("sleep for %s seconds" % timeout)
+        time.sleep(float(timeout))
 
     logging.info("all done")
     sys.exit()
