ccde285acc5385259fa0da570756c2b8d596db8c
straup <straup@gmail.com>
2013-01-26 14:13:18 -0500

    basic hooks for publish-s3 to use a config file

--- bin/publish-s3.py
+++ bin/publish-s3.py
@@ -6,6 +6,7 @@ import os.path
 import logging
 import subprocess
 import redis
+import ConfigParser
 
 if os.uname() == 'Darwin':
     from watchdog.observers.fsevents import FSEventsObserver as Observer
@@ -16,7 +17,7 @@ from watchdog.events import FileSystemEventHandler
 
 import multiprocessing
 
-def publish(path, opts):
+def publish(path, aws_key, aws_secret, s3_bucket, s3_prefix):
 
     root = os.path.dirname(path)
     fname = os.path.basename(path)
@@ -25,16 +26,16 @@ def publish(path, opts):
 
     args = [
         "s3put",
-        "-a", opts.accesskey,
-        "-s", opts.secret,
-        "-b", opts.bucket,
+        "-a", aws_key,
+        "-s", aws_secret,
+        "-b", s3_bucket,
         "-g", "public-read",
         "-p", root,
         ]
 
-    if opts.prefix:
+    if s3_prefix != '':
         args.append("--key_prefix")
-        args.append(opts.prefix)
+        args.append(s3_prefix)
 
     args.append(path)
 
@@ -46,10 +47,10 @@ def publish(path, opts):
 
     os.unlink(path)
 
-    aws_path = "%s/%s" % (opts.bucket, fname)
+    aws_path = "%s/%s" % (s3_bucket, fname)
     url = 'http://s3.amazonaws.com/%s' % aws_path
 
-    key = "loopr_%s" % opts.bucket.replace(".", "_")
+    key = "loopr_%s" % s3_bucket.replace(".", "_")
 
     try:
         r = redis.Redis()
@@ -67,6 +68,11 @@ class Eyeballs(FileSystemEventHandler):
         self.observer = observer
         self.opts = opts
 
+        self.cfg = ConfigParser.ConfigParser()
+        self.cfg.read(opts.config)
+
+        self.watch = self.cfg.get('publish-s3', 'watch')
+
     def on_any_event(self, event):
 
         if event.event_type != 'created':
@@ -74,8 +80,14 @@ class Eyeballs(FileSystemEventHandler):
 
         path = event.src_path
 
-        # publish(path, opts)
-        pool.apply_async(publish, (path, opts))
+        # Really? Just pass cfg, maybe?
+
+        aws_key = self.cfg.get('publish-s3', 'aws_key')
+        aws_secret = self.cfg.get('publish-s3', 'aws_secret')
+        s3_bucket = self.cfg.get('publish-s3', 's3_bucket')
+        s3_prefix = self.cfg.get('publish-s3', 's3_prefix')
+
+        pool.apply_async(publish, (path, aws_key, aws_secret, s3_bucket, s3_prefix))
         
 
 if __name__ == '__main__':
@@ -83,12 +95,15 @@ if __name__ == '__main__':
     import optparse
 
     parser = optparse.OptionParser()
-    parser.add_option("-w", "--watch", dest="watch", help="", default=None)
-    parser.add_option("-a", "--accesskey", dest="accesskey", help="", default=None)
-    parser.add_option("-s", "--secret", dest="secret", help="", default=None)
-    parser.add_option("-b", "--bucket", dest="bucket", help="", default=None)
-    parser.add_option("-p", "--prefix", dest="prefix", help="", default=None)
+    parser.add_option('-c', '--config', dest='config', action='store', help='path to a loopr config file - see source code for a sample config')
     parser.add_option("-v", "--verbose", dest="verbose", action="store_true", help="enable chatty logging; default is false", default=False)
+
+    # MAYBE: allow CLI flags to override the config file?
+    # parser.add_option("-w", "--watch", dest="watch", help="", default=None)
+    # parser.add_option("-a", "--accesskey", dest="accesskey", help="", default=None)
+    # parser.add_option("-s", "--secret", dest="secret", help="", default=None)
+    # parser.add_option("-b", "--bucket", dest="bucket", help="", default=None)
+    # parser.add_option("-p", "--prefix", dest="prefix", help="", default=None)
 
     (opts, args) = parser.parse_args()
 
@@ -100,7 +115,7 @@ if __name__ == '__main__':
     observer = Observer()
     event_handler = Eyeballs(observer, opts)
 
-    observer.schedule(event_handler, path=opts.watch, recursive=False)
+    observer.schedule(event_handler, path=event_handler.watch, recursive=False)
     observer.start()
 
     try:
