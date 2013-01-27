2c16350a99b05de2014eb2cbeacd25e29d230b81
straup <straup@gmail.com>
2013-01-27 09:18:48 -0500

    update the example www/viewing stuff to do fullscreen and display date

--- www/javascript/loopr.js
+++ www/javascript/loopr.js
@@ -1,4 +1,14 @@
 function loopr_init(){
+
+	// fullscreen polyfill stuff
+    
+	var btn = document.getElementById("fullscreen");
+	var loop = document.getElementById("loopr"); 
+
+	btn.addEventListener("click", function() {
+		loop.requestFullscreen();
+	}, false);
+
 	loopr_load();
 }
 
@@ -14,13 +24,24 @@ function loopr_load(){
 function loopr_show(loops){
 
 	var img = loops.pop()
-	console.log(img);
 
 	$("#offstage").attr("src", img);
 
 	$("#offstage").load(function(){
 		$("#loopr").attr("src", img);
 		$("#offstage").attr("src", "");
+
+		var m = img.match(/\/loopr-(\d+)\.gif$/);
+		var ts = m[1];
+
+		var dt = new Date(ts * 1000);
+		var h = dt.getHours();
+		var m = dt.getMinutes();
+
+		if (h < 10){ h = "0" + h; }
+		if (m < 10){ m = "0" + m; }
+
+		$("#when").html(h + "H" + m);
 	});
 
 	setTimeout(function(){
